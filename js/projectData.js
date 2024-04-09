a9d39b0e8b813b8ea2e3a83f2d569134587c3de8
IsabellaaaaShi <157088411+IsabellaaaaShi@users.noreply.github.com>
2024-04-08 23:17:27 -0400

    js folder

---
+++ js/projectData.js
@@ -0,0 +1,25 @@
+// Define 
+const projects = {
+
+    "Project1": {
+        "title": "Project 1 Title",
+        "description": "Project 1 Description",
+        numContent: 3,
+        "imageFileDirectory": "./assets/Project1"
+    },
+
+    "Project2": {
+        "title": "Project 2 Title",
+        "description": "Project 2 Description",
+        numContent: 2,
+        "imageFileDirectory": "./assets/Project2"
+    },
+
+    "Project3": {
+        "title": "Project 3 Title",
+        "description": "Project 3 Description",
+        numContent: 10,
+        "imageFileDirectory": "./assets/Project3"
+    },
+ 
+};
